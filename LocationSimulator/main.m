73fa94f113ccdc539cb875d3e98ba953848628de
Jaanus Siim <jaanus@jaanussiim.com>
2012-10-30 13:30:31 +0100

    Initial project files

---
+++ LocationSimulator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LocationSimulator
+//
+//  Created by Jaanus Siim on 10/30/12.
+//  Copyright (c) 2012 JaanusSiim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
